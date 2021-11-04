4d58dce0afdadecbb8e80c4929ef9236017bbf19
ark <39788908+arkhyeon@users.noreply.github.com>
2021-11-04 17:44:43 +0900

    nginx

---
+++ src/Hello.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hello = ({title, content}) => {
+    return (
+        <div>
+            <h3>{title}</h3>
+            <p>{content}</p>
+        </div>
+    )
+}
+
+export default Hello
